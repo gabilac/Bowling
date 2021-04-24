32bfb247fc8b9ac7ac6bbf592cdc02ce73651b89
gabilac <yogabbagabba46@gmail.com>
2021-04-24 06:54:14 -0600

    Scaffolding

---
+++ Bowl/Models/ZtblSkipLabel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Bowl.Models
+{
+    public partial class ZtblSkipLabel
+    {
+        public long LabelCount { get; set; }
+    }
+}
